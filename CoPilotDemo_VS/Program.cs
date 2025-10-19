46cc0156614c6ee7f3c747a553fea612fd362d17
Yigit_Mesut <41824856+Mesut66@users.noreply.github.com>
2025-10-20 00:52:21 +0300

    Repository Crud

--- CoPilotDemo_VS/Program.cs
+++ CoPilotDemo_VS/Program.cs
@@ -1,6 +1,7 @@
 using CoPilotDemo_VS.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using CoPilotDemo_VS.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ProductRepository>();
 
 var app = builder.Build();
 
---
+++ CoPilotDemo_VS/Service/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using CoPilotDemo_VS.Data;
+using CoPilotDemo_VS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoPilotDemo_VS.Service
+{
+    public class ProductRepository
+    {
+        //async crud operations for Product entity
+        private readonly ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            return await _context.Product.ToListAsync();
+        }
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _context.Product.FindAsync(id);
+        }
+        public async Task AddProductAsync(Product product)
+        {
+            _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Product.Update(product);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
